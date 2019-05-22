e8ee29f55339f3b7201f03e7ed91e783fd490752
“Nada2222git config --global user.name “Nada2222 <nadamohamad498@yahoo.com>
2019-05-22 02:52:17 +0200

    first commit

---
+++ ruby.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+## Print some variables
+puts ENV['PATH']
+#puts ENV['EDITOR']
+## Change a variable then launch a new program
+#ENV['EDITOR'] = 'gedit'
+#`cheat environment_variables --add`
